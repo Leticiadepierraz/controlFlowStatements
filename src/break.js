108937725f87e974f945b0715068cab798783a3d
Leticia Dépierraz <leticia.depierraz@eduvaud.ch>
2024-09-13 14:39:25 +0200

    feat: break on time"

--- src/break.js
+++ src/break.js
@@ -4,5 +4,12 @@ for (let i = 0; i < 10; i++) {
 }
 
 for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 50) {
+        console.log("nombre superieur à 50 ");
+        break;
+    }
+
+    // écrire dans le tableau excel
+
     console.log(numbers[i]);
 }
