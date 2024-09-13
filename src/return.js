44b898c8aab702c33031363cd3521c93eb58718f
Leticia Dépierraz <leticia.depierraz@eduvaud.ch>
2024-09-13 14:41:42 +0200

    feat: break on time"

--- src/return.js
+++ src/return.js
@@ -2,7 +2,7 @@ const numbers = [];
 for (let i = 0; i < 10; i++) {
     numbers.push(Math.floor(Math.random() * 100));
 }
-
+//
 for (let i = 0; i < numbers.length; i++) {
     console.log(numbers[i]);
 }
